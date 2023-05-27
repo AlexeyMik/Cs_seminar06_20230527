aa35661b2b39ed30caa58073ba6dbd66077fbafc
Alexey_mik <mikmav.mik@gmail.com>
2023-05-27 23:58:46 +0300

    дом задание к семинару 20230527

---
+++ HomeWork01_task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void task41()
+{
+    int nOfNumbers = ReadInt("Сколько чисел будете вводить?: ");
+    int CountPositive = 0;
+    for (int i = 0; i < nOfNumbers; i++)
+    {
+        if (ReadInt("Задайте следующее число: ") > 0)
+        {
+            CountPositive++;
+        };
+    }
+    Console.WriteLine($"Введено {CountPositive} положительных чисел");
+}
+int ReadInt(string text) // запрашивает число для ввода с консоли и возвращает его после проглатывания с консоли
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+task41();
